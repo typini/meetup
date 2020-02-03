15797f59b1f0c8c7b43506fbdcf8793e0812672b
typini <typini@gmail.com>
2020-02-02 19:46:21 -0700

    Atatus monitoring incorporated into app.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+atatus.config('0e41e2528c524998beeb40a2fdf7c6a0').install();
+
+atatus.notify(new Error('Test Atatus Setup'));
