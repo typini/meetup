c5304a59a9e8742a9cb02ae8f69e71bc2c821688
typini <typini@gmail.com>
2020-02-03 13:54:55 -0700

    Error messages now in place.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
   state = {
@@ -10,7 +11,18 @@ class CitySearch extends Component {
   handleInputChanged = (event) => {
     const value = event.target.value;
     this.setState({ query: value });
-    getSuggestions(value).then(suggestions => this.setState({ suggestions }));
+    getSuggestions(value).then(suggestions => {
+      this.setState({ suggestions });
+      if (value && suggestions.length === 0){
+        this.setState({
+          infoText: 'We cannot find the city you are looking for.  Please try another city.'
+        });
+      } else {
+        this.setState({
+          infoText: ''
+        });
+      }
+    });
   }
 
   handleItemClicked = (value, lat, lon) => {
@@ -21,6 +33,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input type="text" className="city" value={this.state.query} onChange={this.handleInputChanged} />
         <ul className="suggestions">
           {this.state.suggestions.map(item =>
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
+  state = {
+    errorText: ''
+  }
 
   handleInputChanged = (e) => {
-    this.props.updatePage(e.target.value)
+    this.props.updatePage(e.target.value);
+    if(e.target.value < 1){
+      this.setState({
+        errorText: 'Number should be at least 1'
+      });
+    } else {
+      this.setState({
+        errorText: ''
+      });
+    }
   }
 
   render() {
     return (
       <div className="numberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
         <span>Show</span>
         <input className="eventBox" id="eventBox" type="number" value={this.props.page} onChange={this.handleInputChanged.bind(this)} />
         <span>Events</span>
